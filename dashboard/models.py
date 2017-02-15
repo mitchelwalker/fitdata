d5e9ffc71356ab475438dd210622cae0b8e2680b
th3gre3k <walker.mitchel@gmail.com>
2017-02-14 19:11:10 -0700

    updates

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,12 +1,19 @@
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
 
 from django.db import models
 
 # Create your models here.
 
+class user
+
 class fitdata(models.Model):
+    user = models.ForeignKey(User, unique=True)
     date = models.DateField(default=None)
     steps = models.IntegerField()
     distance = models.IntegerField()
+
+class weightdata(models.Model):
+    user = models.ForeignKey(User, unique=True
     weight = models.IntegerField()
     fat = models.IntegerField()
